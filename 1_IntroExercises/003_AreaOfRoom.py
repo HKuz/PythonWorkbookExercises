3b94e7622f537a6fb1c079751adaa6334e040ad6
Heather Kusmierz <heather.kusmierz@gmail.com>
2021-02-18 11:05:13 -0500

    feat: complete exercise 3 area of a room

---
+++ 1_IntroExercises/003_AreaOfRoom.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+width = float(input('Enter the width of your room in feet: '))
+
+length = float(input('Enter the length of your room in feet: '))
+
+area = width * length
+
+print(f'The area of your room is {area:.1f} square feet.')
