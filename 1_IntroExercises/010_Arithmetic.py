8dd5b6d2063870b17ab4a838053bcd2037079b92
Heather Kusmierz <heather.kusmierz@gmail.com>
2021-02-22 16:05:35 -0500

    feat: update exercise 10 for second page of instructions

--- 1_IntroExercises/010_Arithmetic.py
+++ 1_IntroExercises/010_Arithmetic.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
+import math
+
 a = int(input('Enter integer a: '))
 b = int(input('Enter integer b: '))
 
-print(f'The sum of a+b = {a + b}')
-print(f'The difference of a-b = {a - b}')
-print(f'The product of a*b = {a * b}')
+print(f'The sum of {a}+{b} = {a + b}')
+print(f'The difference of {a}-{b} = {a - b}')
+print(f'The product of {a}*{b} = {a * b}')
+print(f'The quotient of {a}/{b} = {divmod(a, b)[0]}')
+print(f'The remainder of {a}/{b} = {divmod(a, b)[1]}')
+print(f'The log (base 10) of {a} = {math.log10(a):.3f}')
+print(f'The result of {a}^{b} = {a**b}')
