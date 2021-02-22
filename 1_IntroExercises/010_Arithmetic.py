5960d3cf11f8d06c9ae4076e0ecfaf48a4b99746
Heather Kusmierz <heather.kusmierz@gmail.com>
2021-02-22 15:58:52 -0500

    feat: complete exercise 10 arithmetic

---
+++ 1_IntroExercises/010_Arithmetic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+a = int(input('Enter integer a: '))
+b = int(input('Enter integer b: '))
+
+print(f'The sum of a+b = {a + b}')
+print(f'The difference of a-b = {a - b}')
+print(f'The product of a*b = {a * b}')
